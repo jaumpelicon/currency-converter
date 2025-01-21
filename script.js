0fefb0ffec90057d112d49c916a88b13a496ea0c
Joao Pelicon <peliconjoao@gmail.com>
2025-01-21 08:35:06 -0300

    feat- using logic to show total value converted

--- script.js
+++ script.js
@@ -1,10 +1,13 @@
 const form = document.querySelector("form")
-const inputValue = document.getElementById("amount")
+const input = document.getElementById("amount")
+const result = document.getElementById("result")
+const footer = document.querySelector("main footer")
+const description = document.getElementById("description")
 const currencySelector = document.getElementById("currency")
 
-const dolarCurrency = 6.1921
+const dolarCurrency = 6.19
 const euroCurrency = 6.23
-const libraCurrency = 7.7887
+const libraCurrency = 7.79
 
 amount.addEventListener("input", () => {
   const hasCharcters = /\D+/g
@@ -14,10 +17,11 @@ amount.addEventListener("input", () => {
 form.onsubmit = (event) => {
   event.preventDefault()
 
-  const totalValue = inputValue.value
+  const totalValue = input.value
+  const symbol = getCurrencySymbol(currencySelector.value)
   const currencyValue = getCurrencyValue(currencySelector.value)
 
-  convertCurrency(totalValue, currencyValue)
+  convertCurrency(totalValue, currencyValue, symbol)
 }
 
 function getCurrencyValue(symbol) {
@@ -29,7 +33,35 @@ function getCurrencyValue(symbol) {
   }
 }
 
-function convertCurrency(totalValue, price) {
-  const convertedValue = Number(totalValue) / price
-  alert(`Valor convertido: ${convertedValue.toFixed(2)}`)
+function getCurrencySymbol(symbol) {
+  switch (symbol) {
+    case "USD": return 'US$'
+    case "EUR": return '€'
+    case "GBP": return '£'
+    default: return 1
+  }
+}
+
+function formatBRCurrency(value) {
+  return Number(value).toLocaleString('pt-BR', {
+    style: 'currency',
+    currency: 'BRL'
+  })
+}
+
+function convertCurrency(totalValue, price, symbol) {
+
+  try {
+    const convertedValue = Number(totalValue) * price
+    description.textContent = `${symbol} 1 = ${formatBRCurrency(price)}`
+    result.textContent = formatBRCurrency(convertedValue)
+    footer.classList.add("show-result")
+
+  } catch (error) {
+    footer.classList.remove("show-result")
+
+    console.error("Error: ", error)
+
+    alert("Não foi possível converter a moeda, tente novamente.")
+  }
 }
